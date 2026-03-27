4465edbd5f404af3c65d78e4e52da4dc9ee92d09
nhathuymario <huyne1501@gmail.com>
2026-03-27 23:09:25 +0700

    NG-2 Product exception

---
+++ backend/Product/src/main/java/uth/nhathuy/Product/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package uth.nhathuy.Product.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
