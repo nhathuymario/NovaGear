b4b7969873780c60262088737bbff7f6e22f7026
nhathuymario <huyne1501@gmail.com>
2026-03-30 23:30:38 +0700

    fix

--- gateway/gateway/src/main/java/uth/nhathuy/gateway/config/SecurityConfig.java
+++ gateway/gateway/src/main/java/uth/nhathuy/gateway/config/SecurityConfig.java
@@ -12,8 +12,13 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Vô hiệu hóa CSRF
+                .cors(ServerHttpSecurity.CorsSpec::disable) // Tạm thời disable CORS ở Security để dùng cấu hình ở .yml
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/auth/**").permitAll()
+                        .pathMatchers("/api/products/**").permitAll() // Cho phép công khai
+                        .anyExchange().authenticated()
+                )
                 .build();
     }
 }
