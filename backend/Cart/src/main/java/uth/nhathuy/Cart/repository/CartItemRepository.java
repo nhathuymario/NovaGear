f648d38fbbf50be4ae90fc5f87143aac278b02c7
nhathuymario <huyne1501@gmail.com>
2026-03-30 20:37:49 +0700

    NG-4 repository

---
+++ backend/Cart/src/main/java/uth/nhathuy/Cart/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package uth.nhathuy.Cart.repository;
+
+import uth.nhathuy.Cart.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartIdAndProductIdAndVariantId(Long cartId, Long productId, Long variantId);
+}
