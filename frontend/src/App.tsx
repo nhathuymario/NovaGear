803faa2bd5fc154640ab83ad04183e61c16290a4
nhathuymario <huyne1501@gmail.com>
2026-03-30 23:37:38 +0700

    --amend --no-edit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import MainLayout from "./components/layout/MainLayout"
+import HomePage from "./pages/HomePage"
+import ProductListPage from "./pages/ProductListPage"
+import ProductDetailPage from "./pages/ProductDetailPage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import CartPage from "./pages/CartPage"
+
+export default function App() {
+    return (
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductListPage />} />
+                <Route path="/products/:id" element={<ProductDetailPage />} />
+                <Route path="/cart" element={<CartPage />} />
+            </Route>
+
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+    )
+}
