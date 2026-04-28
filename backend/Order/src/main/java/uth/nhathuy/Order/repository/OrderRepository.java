7f11e5475d351e7440c048649133a0ee0c1ff624
nhathuymario <huyne1501@gmail.com>
2026-04-29 00:05:18 +0700

    order repository

--- backend/Order/src/main/java/uth/nhathuy/Order/repository/OrderRepository.java
+++ backend/Order/src/main/java/uth/nhathuy/Order/repository/OrderRepository.java
@@ -4,7 +4,10 @@ import uth.nhathuy.Order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUserIdOrderByCreatedAtDesc(Long userId);
+
+    Optional<Order> findByIdAndUserId(Long id, Long userId);
 }
