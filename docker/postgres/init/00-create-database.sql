de5ff51c2d35b645260423c61248475040446003
nhathuymario <huyne1501@gmail.com>
2026-04-28 16:26:58 +0700

    add sql docker

---
+++ docker/postgres/init/00-create-database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE user_db;
+CREATE DATABASE product_db;
+CREATE DATABASE cart_db;
+CREATE DATABASE inventory_db;
+CREATE DATABASE order_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE notification_db;
