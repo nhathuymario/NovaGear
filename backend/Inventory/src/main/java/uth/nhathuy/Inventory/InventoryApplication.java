9298375c689ccf8f192b7f32b01fd1ff2cdffd2f
nhathuymario <huyne1501@gmail.com>
2026-04-29 00:04:55 +0700

    Spring Cloud OpenFeign connect Feign Client

--- backend/Inventory/src/main/java/uth/nhathuy/Inventory/InventoryApplication.java
+++ backend/Inventory/src/main/java/uth/nhathuy/Inventory/InventoryApplication.java
@@ -1,9 +1,11 @@
 package uth.nhathuy.Inventory;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InventoryApplication {
 
 	public static void main(String[] args) {
