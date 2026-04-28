e3e3935cbd72808955d84a50f7a242eac1d94ed1
nhathuymario <huyne1501@gmail.com>
2026-04-29 00:08:31 +0700

    Spring Cloud OpenFeign connect Feign Client for order

--- backend/Order/src/main/java/uth/nhathuy/Order/OrderApplication.java
+++ backend/Order/src/main/java/uth/nhathuy/Order/OrderApplication.java
@@ -1,13 +1,15 @@
 package uth.nhathuy.Order;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
 
 }
